9a7edbcc30295f42c6c147baeb60f492c2257ada
Douglas Wallin <wallind@mail.gvsu.edu>
2019-01-30 19:57:23 -0500

    Succesfully pulled root ref from fireB into app.js

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { db } from './src/services/db';
 
 export default class App extends React.Component {
   render() {
+	var testData = db.ref();
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
