c5da832d91d2fe3ca867328b11584319b20c24a9
Douglas Wallin <wallind@mail.gvsu.edu>
2019-01-30 19:25:04 -0500

    Further file organization, 3 folders in source now

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import firebase from "firebase";
 
 export default class App extends React.Component {
   render() {
