d4aeca9dea0fc05c210d53bf1ce419087aa4b125
Douglas Wallin <wallind@mail.gvsu.edu>
2019-01-30 19:18:18 -0500

    Re-organization of files (firebase config)

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from "firebase";
 
 export default class App extends React.Component {
   render() {
