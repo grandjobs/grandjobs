bf47a48a2defefdbac59ceb9e8ded74619a3418e
Douglas Wallin <wallind@mail.gvsu.edu>
2019-02-01 15:51:12 -0500

    Continued work in the dataGen.js file

--- src/services/dataGen.js
+++ src/services/dataGen.js
@@ -14,11 +14,28 @@ const db = app.database();
 
 var rootRef = db.ref();
 
-rootRef.once('value', function(snapshot) {
-	if (!snapshot.hasChild("USERS")) {
-		console.log("here brah");
-		rootRef.child.set("USERS");
-	} else {
-		console.log("why..");
-	}
+//rootRef.once('value', function(snapshot) {
+//	if (!snapshot.hasChild("USERS")) {
+//		console.log("here brah");
+//		rootRef.child.set("USERS");
+//	} else {
+//		console.log("why..");
+//	}
+//});
+
+var userRef = rootRef.child("USERS");
+//userRef.push().set({ username: "jokes" });
+
+fetch('https://randomuser.me/api?results=10&inc=login,cell')
+	.then(r => r.json())
+	.then(u => {
+		var users = u.results;
+		
+		for (let i = 0; i < users.length; i++) {
+				userRef.push().set({ username: users[i]['login']['username'] });
+		}
+		//console.log(users.length);
+		//console.log(users[0]);
+		//console.log(users[0]['login']['username']);
+		//console.log(users);
 });
