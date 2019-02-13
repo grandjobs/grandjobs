8538e1bdc92940d25aa8e08b1ac32b9ad61644a4
Ryan Walt <waltr@mail.gvsu.edu>
2019-02-13 15:13:08 -0500

    Add files via upload
    Fixing master....

---
+++ AccountSetup.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, TextInput } from 'react-native';
+import Button from 'react-native-button';
+// import { TextInput } from 'react-native-paper';
+import PhoneInput from 'react-native-phone-input';
+import { Actions } from 'react-native-router-flux';
+
+
+export default class AccountSetup extends React.Component {
+    render() {
+        return (
+            <View style={styles.mainContainer}>
+                <View style={styles.textContainer}>
+                    <Text style={styles.largeText}>Awesome!</Text>
+                    <Text style={styles.mainText}>Lets fill out some information.</Text>
+                </View>
+                <View style={styles.fillContainer}>
+                    {/*// <Text style={{textAlign: 'right', color: 'white'}}>Phone:</Text> */}
+                    {/* // <PhoneInput ref='phone' style={styles.inputPhone} textStyle={{fontSize: 18, color:'white', fontFamily: 'sans-serif-thin', }}/>*/}
+                    <TextInput style={styles.inputText}
+                        keyboardType='numeric'
+                        selectTextOnFocus={true}
+                        placeholder='Phone #'
+                    />
+                    <TextInput style={styles.inputText}
+                        selectTextOnFocus={true}
+                        placeholder='First Name (Optional)'
+                    />
+                    <TextInput style={styles.inputText}
+                        selectTextOnFocus={true}
+                        placeholder='Last Name (Optional)'
+                    />
+                    <TextInput style={styles.inputText}
+                        selectTextOnFocus={true}
+                        placeholder='Email (Optional)'
+                    />
+                </View>
+                <View style={styles.bottomContainer}>
+                    <Button style={styles.buttonDesign} onPress={()=>this.nextPressed()}>
+                    Next
+                    </Button>
+                </View>
+
+            </View>
+        );
+    }
+
+    nextPressed(){
+        console.log('Next pressed');
+        Actions.SkillPage();
+    }
+
+}
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        backgroundColor: '#1E2027',
+        // padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textContainer:{
+        top: Dimensions.get('window').height * 0.05
+    },
+    fillContainer:{
+        top: Dimensions.get('window').height * 0.20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    largeText:{
+        fontSize: 65,
+        textAlign: 'center',
+        top: 20,
+        color: '#d6d6d6',
+        fontFamily: 'sans-serif-thin'
+    },
+    mainText:{
+        fontSize: 25,
+        textAlign: 'center',
+        top: 20,
+        color: '#d6d6d6',
+        fontFamily: 'sans-serif-thin'
+    },
+    inputText:{
+        borderColor: '#fff',
+        color: '#fff',
+        fontSize: 25,
+        textAlign: 'center',
+        color:'white',
+        fontFamily: 'sans-serif-thin',
+        padding: 10,
+        margin: 20,
+        borderWidth: 1,
+        borderRadius: 30,
+        width: Dimensions.get('window').width * 0.8
+    },
+    inputPhone:{
+        borderColor: '#fff',
+        color: '#fff',
+        padding: 12,
+        margin: 20,
+        borderWidth: 1,
+        borderRadius: 30,
+        textAlign: 'center',
+        width: Dimensions.get('window').width * 0.8,
+    },
+    bottomContainer:{
+        flex: 1,
+        justifyContent: 'flex-end',
+        marginBottom: 0
+    },
+    buttonDesign:{
+        fontSize: 20,
+        fontFamily: 'sans-serif-thin',
+        padding: 10,
+        margin: 30,
+        width: 150,
+        color: '#fff',
+        borderRadius: 30,
+        borderColor: '#a9fcd4',
+        borderWidth: 1,
+    },
+});
