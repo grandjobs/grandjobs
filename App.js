f42b7993c731725e5e8c0a6a1c659bdf33c21027
Douglas Wallin <wallind@mail.gvsu.edu>
2019-02-13 11:46:38 -0500

    commit to checkout firebase-setup

--- App.js
+++ App.js
@@ -1,26 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-=======
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import Button from 'react-native-button'
 import {Router, Scene} from 'react-native-router-flux';
@@ -66,4 +44,3 @@ const App = () => {
 // const App = createAppNavigator(MainNavigator);
 
 export default App;
->>>>>>> 3c50dbfca8b7bdd7ad52a93d3ad260f45c140aa2
